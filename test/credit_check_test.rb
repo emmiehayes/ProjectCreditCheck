b9f32523e9fb22d1a9841b8a651f2cb846c653be
Emmie Hayes <emmiehayes2@gmail.com>
2018-03-18 17:58:11 -0600

    Test Added- Invalid, Amex Valid, Amex Invalid

--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -10,7 +10,7 @@ class CreditCheckTest < Minitest::Test
     assert_instance_of CreditCheck, credit_check
   end
 
-  def test_card
+  def test_card_valid
     credit_check = CreditCheck.new(4929735477250543)
     assert_equal 4929735477250543, credit_check.card
   end
@@ -21,13 +21,11 @@ class CreditCheckTest < Minitest::Test
     assert_equal result, credit_check.integer_to_reversed_array
   end
 
-
   def test_double_every_other_element
     credit_check = CreditCheck.new(4929735477250543)
     result = [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8]
     assert_equal result, credit_check.double_every_other_element
   end
-
 
   def test_sum_numbers_over_nine
     credit_check = CreditCheck.new(4929735477250543)
@@ -35,15 +33,28 @@ class CreditCheckTest < Minitest::Test
     assert_equal result, credit_check.sum_numbers_over_nine
   end
 
-
   def test_sum_of_array
     credit_check = CreditCheck.new(4929735477250543)
     assert_equal 80, credit_check.sum_of_array
   end
 
-
-  def test_validate_card_number
+  def test_validate_card_number_valid
     credit_check = CreditCheck.new(4929735477250543)
     assert_equal "The number is valid!", credit_check.validate_card_number
+  end
+
+  def test_validate_card_number_invalid
+    credit_check = CreditCheck.new(5541801923795240)
+    assert_equal "The number is invalid.", credit_check.validate_card_number
+  end
+
+  def test_validate_amex_valid
+    credit_check = CreditCheck.new(342804633855673)
+      assert_equal "The number is valid!", credit_check.validate_card_number
+  end
+
+  def test_validate_amex_invalid
+    credit_check = CreditCheck.new(342801633855673)
+      assert_equal "The number is invalid.", credit_check.validate_card_number
   end
 end
