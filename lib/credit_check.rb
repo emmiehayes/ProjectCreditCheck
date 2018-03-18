d636676c384fc30b3095cecee77131122c23884c
Emmie Hayes <emmiehayes2@gmail.com>
2018-03-18 17:07:12 -0600

    Remove notes

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -46,37 +46,3 @@ class CreditCheck
   end
 
 end
-
-
-# new_card = "4929735477250543"
-# card_1 = CreditCheck.new(new_card)
-
-
-
-
-
-
-
-
-
-
-
-
-# print "Please enter your 16 digit credit card number:"
-# card_number = gets.chomp.to_i
-# card_number
-
-# if card_number.length != 16
-#  puts "Invalid card number."
-# end
-#
-# p card_number
-
-##PSEUDOCODE-------------------------------------------------------------------
-
-#DONE card_number = gets.chomp.to_i will automatically convert the string input to integer form
-#DONE simplify dramatic split, reverse, map method chain from yesterday by using newly discovered method .digits
-## DONE need to def method to multiply the numbers with an odd index by 2, calling a method to a method
-## DONE need to map over array. if number is > 9 subtract 9 from the number to return the same number that adding the two digits would return.
-## DONE need to calculate the sum of the array (returning the sum as a new variable) call method on method object.
-## DONE need to confirm that the sum of the array % 10 equals zero to check validity, no need for puts
